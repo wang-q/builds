ae32aa19417e7661bf7158bedfe6d1e44fa6c69e
Qiang Wang <wang-q@outlook.com>
2025-02-17 14:26:56 +0800

    MERQURY.FK.linux.tar.gz

--- script/MERQURY.FK.sh
+++ script/MERQURY.FK.sh
@@ -22,37 +22,39 @@ elif [ "$OS_TYPE" == "macos" ]; then
 fi
 
 # Enter the MERQURY.FK project directory
-cd MERQURY.FK
+cd MERQURY.FK || exit 1
 
 # Restore the Git repository to its original state
-git restore .
+git restore . || exit 1
 
 # Clean the build environment
 make clean
 
 # Modify the Makefile to use zig cc and specify the target architecture
-sed -i 's/^\t\s*gcc/\t$(CC)/g' Makefile
-sed -i "s|^CFLAGS =.*$|CFLAGS = -I../static/include -L../static/lib -O3 -Wall -Wextra -Wno-unused-result -fno-strict-aliasing|g" Makefile
-sed -i "1i CC = zig cc -target ${TARGET_ARCH}" Makefile
+sed -i 's/^\t\s*gcc/\t$(CC)/g' Makefile || exit 1
+sed -i "s|^CFLAGS =.*$|CFLAGS = -I../static/include -L../static/lib -O3 -Wall -Wextra -Wno-unused-result -fno-strict-aliasing|g" Makefile || exit 1
+sed -i "1i CC = zig cc -target ${TARGET_ARCH}" Makefile || exit 1
 
 # Build the project
-make
+make || exit 1
 
-# Define the name of the compressed file
+# Get binary names from Makefile
+BINS=$(make -p | grep "^all: " | sed 's/^all: //')
+
+# Define archive name based on OS type
 FN_TAR="MERQURY.FK.${OS_TYPE}.tar.gz"
 
-# Package the build results
-GZIP=-9 tar cvfz ${FN_TAR} \
-    $(make -p | grep "^all: " | sed 's/^all: //')
+# Create compressed archive with maximum compression
+tar -cf - ${BINS} | gzip -9 > ${FN_TAR}
 
-# Move the compressed file to the tar directory
+# Move archive to the central tar directory
 mv ${FN_TAR} ../tar/
 
-# Restore the Git repository and clean the build environment
+# Clean up build environment
 git restore .
 make clean
 
-# Return to the parent directory and commit the compressed file to the Git repository
+# Commit the new archive
 cd ..
 git add "tar/${FN_TAR}"
 git commit -a -m "${FN_TAR}"
