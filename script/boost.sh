907001da8763c0aa7305f9dfdead53026e3cfd87
Qiang Wang <wang-q@outlook.com>
2025-02-25 22:10:40 +0800

    Update boost.sh

--- script/boost.sh
+++ script/boost.sh
@@ -6,26 +6,53 @@ source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 # Extract source code
 extract_source
 
+# # Configure CMake with Zig compiler
+# ./bootstrap.sh \
+#     --with-toolset=clang \
+#     --without-icu \
+#     --without-libraries=python,mpi,log
+
+# echo "using clang : zig : zig c++ ;" \
+#     > user-config.jam
+
+# ./b2 headers
+
+# ./b2 \
+#     --prefix="${TEMP_DIR}/collect" \
+#     -d2 \
+#     -j8 \
+#     --layout=system \
+#     --user-config=user-config.jam \
+#     threading=multi \
+#     link=shared,static \
+#     install
+
 # Configure CMake with Zig compiler
-./bootstrap.sh \
-    --with-toolset=clang \
-    --without-icu \
-    --without-libraries=python,mpi,log
+ASM="zig cc" \
+CC="zig cc" \
+CXX="zig c++" \
+cmake \
+    -DCMAKE_ASM_COMPILER_TARGET="${TARGET_ARCH}" \
+    -DCMAKE_C_COMPILER_TARGET="${TARGET_ARCH}" \
+    -DCMAKE_CXX_COMPILER_TARGET="${TARGET_ARCH}" \
+    -DBOOST_ROOT="${PWD}" \
+    -DCMAKE_INSTALL_PREFIX="${TEMP_DIR}/collect" \
+    -DBOOST_ENABLE_PYTHON=OFF \
+    -DBOOST_ENABLE_MPI=OFF \
+    -DBOOST_IOSTREAMS_ENABLE_BZIP2=OFF \
+    -DBOOST_IOSTREAMS_ENABLE_LZMA=OFF \
+    -DBOOST_IOSTREAMS_ENABLE_ZSTD=OFF \
+    -DBOOST_LOCALE_ENABLE_ICU=OFF \
+    -S . -B build
 
-echo "using clang : zig : zig c++ ;" \
-    > user-config.jam
+# # Build the project
+# cmake --build build -- -j 8 || exit 1
 
-./b2 headers
+# # Collect binaries
+# collect_bins "build/bin/spoa"
 
-./b2 \
-    --prefix="${TEMP_DIR}/collect" \
-    -d2 \
-    -j8 \
-    --layout=system \
-    --user-config=user-config.jam \
-    threading=multi \
-    link=shared,static \
-    install
+# # Use build_tar function from common.sh
+# build_tar
 
 # # # Build the project
 # # cmake --build build -- -j 8 || exit 1
