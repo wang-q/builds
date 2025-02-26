cf65647d74144d37847cc5571f700298b62e055b
Qiang Wang <wang-q@outlook.com>
2025-02-27 01:31:09 +0800

    zig

--- script/bcalm.sh
+++ script/bcalm.sh
@@ -9,13 +9,27 @@ extract_source
 cmake -LH .
 
 # Configure CMake with Zig compiler
+ASM="zig cc" \
+CC="zig cc" \
+CXX="zig c++" \
+CFLAGS="-I$HOME/bin/include" \
+CXXFLAGS="-I$HOME/bin/include" \
+LDFLAGS="-L$HOME/bin/lib" \
 cmake \
+    -DCMAKE_ASM_COMPILER_TARGET="${TARGET_ARCH}" \
+    -DCMAKE_C_COMPILER_TARGET="${TARGET_ARCH}" \
+    -DCMAKE_CXX_COMPILER_TARGET="${TARGET_ARCH}" \
+    -DCMAKE_INCLUDE_PATH="$HOME/bin/include" \
+    -DCMAKE_LIBRARY_PATH="$HOME/bin/lib" \
+    -DZLIB_INCLUDE_DIR="$HOME/bin/include" \
+    -DZLIB_LIBRARY="$HOME/bin/lib/libz.a" \
+    -DCMAKE_C_FLAGS="-I$HOME/bin/include -Wno-unqualified-std-cast-call -Wno-implicit-function-declaration -Wno-incompatible-function-pointer-types -Wno-deprecated-builtins -Wno-deprecated-declarations" \
+    -DCMAKE_CXX_FLAGS="-I$HOME/bin/include -Wno-unqualified-std-cast-call -Wno-implicit-function-declaration -Wno-incompatible-function-pointer-types -Wno-deprecated-builtins -Wno-deprecated-declarations" \
     -DKSIZE_LIST="32 64 96 128" \
     -DCMAKE_BUILD_TYPE=Release \
     -Wno-dev \
     -DBUILD_TESTING=OFF \
     -S . -B build
-#     # -DBUILD_STATIC_EXECS=ON \
 
 # Build the project
 cmake --build build -- -j 8 || exit 1
