774440bc46f39fd02714f3f2404052161a0c295a
Qiang Wang <wang-q@outlook.com>
2025-02-28 03:10:35 +0800

    Update MaSuRCA.sh

--- script/MaSuRCA.sh
+++ script/MaSuRCA.sh
@@ -10,19 +10,35 @@ extract_source
 export CC="zig cc -target ${TARGET_ARCH}"
 export CXX="zig c++ -target ${TARGET_ARCH}"
 export DEST="${TEMP_DIR}/collect"
+export CFLAGS="-I${HOME}/bin/include -fcommon -Wno-format -Wno-implicit-function-declaration"
+export CXXFLAGS="-I${HOME}/bin/include -fcommon -Wno-format -Wno-implicit-function-declaration"
+export LDFLAGS="-L${HOME}/bin/lib"
+export BOOST_ROOT="${HOME}/bin"
+export BOOST_INCLUDEDIR="${HOME}/bin/include"
+export BOOST_LIBRARYDIR="${HOME}/bin/lib"
 
-bash install.sh
+# bash install.sh
 
-# ./configure \
-#     --prefix="${TEMP_DIR}/collect" \
-#     --bindir="${TEMP_DIR}/collect" \
-#     ${SIMD_OPT} \
-#     --enable-threads \
-#     || exit 1
-# make -j 8 || exit 1
+cd global-1
+
+# ./configure --help
+
+# sed -i 's/m4_map_args_w(\[samtools /m4_map_args_w([/' configure.ac
+
+# tail configure.ac
+
+# ./autoreconf -if
+
+./configure \
+    --prefix="${TEMP_DIR}/collect" \
+    --disable-swig \
+    --disable-dependency-tracking \
+    --disable-silent-rules \
+    || exit 1
+make -j 8 || exit 1
 # make install || exit 1
 
-tree collect
+# tree collect
 
 # # Use build_tar function from common.sh
 # build_tar
